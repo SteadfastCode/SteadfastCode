253543f66b24731bc7d4fe41a83b612b14b23b9f
Daniel Ecker <accounts@theeckers.family>
2025-06-02 23:19:27 -0400

    Updated Contact to not be empty.

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <h1 class="text-center mb-4">About SteadfastCode</h1>
+        <p class="text-center">
+          We build innovative AI solutions to empower small businesses.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #f97316;
+}
+</style>
